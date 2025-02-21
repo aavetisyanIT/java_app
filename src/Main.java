6c55420b5e1293db53fd0395e3e52e4bd9281841
Artur <aavetisyanit@gmail.com>
2025-02-21 15:49:12 -0600

    Handle division by zero use case

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         double value1 = 100.0;
-        double value2 = 30.0;
+        double value2 = 0;
         double result = 0;
 
-        char operator = 'm';
+        char operator = 'd';
 
-        if( operator == 'a') {
+        if (operator == 'a') {
             result = value1 + value2;
         } else if (operator == 's') {
             result = value1 - value2;
         } else if (operator == 'd') {
-            result = value1 / value2;
+            if (value2 != 0) {
+                result = value1 / value2;
+            }
         } else if (operator == 'm') {
             result = value1 * value2;
         } else {
