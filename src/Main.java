2375c1f163f81503467ed0862d03432ce80fb2de
Artur <aavetisyanit@gmail.com>
2025-02-21 15:59:21 -0600

    Refactor code using switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         double value1 = 100.0;
-        double value2 = 0;
+        double value2 = 1;
         double result = 0;
 
         char operator = 'd';
 
-        if (operator == 'a') {
-            result = value1 + value2;
-        } else if (operator == 's') {
-            result = value1 - value2;
-        } else if (operator == 'd') {
-            if (value2 != 0) {
-                result = value1 / value2;
-            }
-        } else if (operator == 'm') {
-            result = value1 * value2;
-        } else {
-            System.out.println("Wrong Operation");
+        switch (operator) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'd':
+                result = value2 != 0 ? value1 / value2 : 0.0d;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            default:
+                System.out.println("Wrong Operator!");
         }
 
         System.out.println(result);
