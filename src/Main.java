cd806f201a4adade50696054057c03cc92509c78
Artur <aavetisyanit@gmail.com>
2025-02-25 15:50:25 -0600

    Create logic to calculate factorial using while loop

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        double value1 = 100.0;
-        double value2 = 1;
-        double result = 0;
+        int result = 1;
+        int currentValue = 5;
 
-        char operator = 'd';
-
-        switch (operator) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value1 - value2;
-                break;
-            case 'd':
-                result = value2 != 0 ? value1 / value2 : 0.0d;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            default:
-                System.out.println("Wrong Operator!");
+        while (currentValue > 1) {
+            result *= currentValue;
+            currentValue--;
         }
 
         System.out.println(result);
