8ff8a967957074af50385054db4c18e5a539e724
Artur <aavetisyanit@gmail.com>
2025-02-25 15:50:47 -0600

    Revert changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int result = 1;
-        int currentValue = 5;
+        double value1 = 100.0;
+        double value2 = 1;
+        double result = 0;
 
-        while (currentValue > 1) {
-            result *= currentValue;
-            currentValue--;
+        char operator = 'd';
+
+        switch (operator) {
+            case 'a':
+                result = value1 + value2;
+                break;
+            case 's':
+                result = value1 - value2;
+                break;
+            case 'd':
+                result = value2 != 0 ? value1 / value2 : 0.0d;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            default:
+                System.out.println("Wrong Operator!");
         }
 
         System.out.println(result);
