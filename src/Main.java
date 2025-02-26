f1b9af3e2c67c09a8c167cd825e13b2c6e45017d
Artur <aavetisyanit@gmail.com>
2025-02-26 16:13:46 -0600

    Utilize arrays and for loop to perform multiple operations

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        double value1 = 100.0;
-        double value2 = 1;
-        double result = 0;
+        double[] left = {100.0d, 20.3d, 50.0d, 9.0d};
+        double[] right = {20.0d, 20.0d, 30.0d, 3.0d};
+        char[] operatorArr = {'d', 'm', 's', 'a'};
 
-        char operator = 'd';
+        double[] resultArr = new double[operatorArr.length];
 
-        switch (operator) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value1 - value2;
-                break;
-            case 'd':
-                result = value2 != 0 ? value1 / value2 : 0.0d;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            default:
-                System.out.println("Wrong Operator!");
+        for (int i = 0; i < operatorArr.length; i++) {
+            switch (operatorArr[i]) {
+                case 'a':
+                    resultArr[i] = left[i] + right[i];
+                    break;
+                case 's':
+                    resultArr[i] = left[i] - right[i];
+                    break;
+                case 'd':
+                    resultArr[i] = right[i] != 0 ? left[i] / right[i] : 0.0d;
+                    break;
+                case 'm':
+                    resultArr[i] = left[i] * right[i];
+                    break;
+                default:
+                    System.out.println("Wrong Operator!");
+            }
         }
 
-        System.out.println(result);
+        for (double result : resultArr)
+            System.out.println(result);
     }
 }
