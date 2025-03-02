c5c4e91bce3497b431df1a67c5e2e4edadcea9be
Artur <aavetisyanit@gmail.com>
2025-03-02 08:35:35 -0600

    Add logic to accept inputs through command line arguments

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,27 @@ public class Main {
 
         double[] resultArr = new double[operatorArr.length];
 
-        for (int i = 0; i < operatorArr.length; i++) {
-            resultArr[i] = execute(operatorArr[i], left[i], right[i]);
+        if (args.length == 0) {
+            for (int i = 0; i < operatorArr.length; i++) {
+                resultArr[i] = execute(operatorArr[i], left[i], right[i]);
+            }
+
+            for (double result : resultArr)
+                System.out.println(result);
+        } else if (args.length == 3) {
+            handleCommandLineArgs(args);
+        } else {
+            System.out.println("Please, provide 3 arguments");
         }
 
-        for (double result : resultArr)
-            System.out.println(result);
+    }
+
+    static void handleCommandLineArgs(String[] args) {
+        char code = args[0].charAt(0);
+        double left = Double.parseDouble(args[1]);
+        double right = Double.parseDouble(args[2]);
+        double result = execute(code, left, right);
+        System.out.println(result);
     }
 
     static double execute(char opCode, double left, double right) {
